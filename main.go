02b17c2f8d09fa40f4d210598e7c9bec3dd5a49c
pbk8s <61557716+pbk8s@users.noreply.github.com>
2020-09-28 14:33:02 -0700

    Update main.go

--- main.go
+++ main.go
@@ -6,8 +6,8 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hi there!!")
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Welcome to our CICD demo! Hope you learn something!")
 	})
 
 	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
